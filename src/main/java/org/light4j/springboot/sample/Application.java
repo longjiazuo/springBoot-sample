bd83a094538f53a4628ee6f807a18032f96aad6c
longjiazuo <812198178@qq.com>
2017-05-24 20:30:10 +0800

    修改类名

--- src/main/java/org/light4j/springboot/sample/Application.java
+++ src/main/java/org/light4j/springboot/sample/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication  //①
-public class HelloApplication {
+public class Application {
 	public static void main(String[] args) {//②
-		SpringApplication.run(HelloApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
